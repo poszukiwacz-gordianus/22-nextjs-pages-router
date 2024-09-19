445a02b5cc3c256b267e6efbf3d175f2fdb5e9dd
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-19 11:28:20 +0200

    Add page titles and favicon

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function Cabin() {
   const router = useRouter();
+  const cabinId = router.query.cabinId;
 
-  return <div>Cabin #{router.query.cabinId}</div>;
+  return (
+    <>
+      <Head>
+        <title>Cabin #{cabinId} | The Wild Oasis</title>
+      </Head>
+      <div>Cabin #{cabinId}</div>
+    </>
+  );
 }
 
 export default Cabin;
